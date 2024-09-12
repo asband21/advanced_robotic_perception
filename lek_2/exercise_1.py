365c71e16097cc40aa8377a27f910356bcfad7aa
klyxa <kykutura@gmx.com>
2024-09-12 16:08:36 +0200

    lek virker ikke

---
+++ lek_2/exercise_1.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+# Open picture
+img_1 = cv2.imread("aau-city-1.jpg")
+img_2 = cv2.imread("aau-city-2.jpg")
+
+#img_1 = np.float32(img_1)
+#img_2 = np.float32(img_2)
+
+sz = img_1.shape
+
+pash = 6
+r = int(pash/2)
+
+sob_x  = np.array([-1, 0, 1])
+sob_y  = np.array([[1, 0, -1]])
+
+ix = cv2.filter2D(img_1,-1, sob_x)
+iy = cv2.filter2D(img_1,-1, sob_y)
+
+for i in range(sz[0] - pash):
+    for j in range(sz[1] - pash):
+        bil_ix = ix[i:i+pash, j:j+pash]
+        bil_iy = iy[i:i+pash, j:j+pash]
+
+ixx = 0
+ixy = 0
+iyx = 0
+iyy = 0
+
+#for i in range(sz[0]):
+#    for j in range(sz[1]):
+
+
+cv2.imshow("img 1", img_1)
+cv2.imshow("img 1 sob x",  ix)
+cv2.imshow("img 1 sob y",  iy)
+cv2.imshow("img 2", img_2)
+
+cv2.waitKey(0)
+
