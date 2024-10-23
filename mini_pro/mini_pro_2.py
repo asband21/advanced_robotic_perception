fc85b94a34971812767094619cb68b3a2b24ad85
klyxa <kykutura@gmx.com>
2024-10-23 13:48:42 +0200

    til fæget lidt

--- mini_pro/mini_pro_2.py
+++ mini_pro/mini_pro_2.py
@@ -28,6 +28,14 @@ for i in range(1, img_0.shape[0]-2):
             if(k >= 0 and k <= img_0.shape[1]-1):
                 mids = abs(Il[k] - Ir[j])
                 pixel_disparity[j][k-j+delta] = mids
+    
+    cv2.imshow("img", pixel_disparity)
+    cv2.imwrite(f"pixel_disparity_{i}.png",pixel_disparity)
+
+    computeIntensityGradientsX(imgL, imgR, scanline, no_igL, no_igR)
+
+
+    cv2.waitKey(0)
 
 cv2.imshow("img 0", img_0)
 cv2.imshow("img 1", img_1)
@@ -39,8 +47,9 @@ cv2.imshow("img 3", img_3)
 cv2.waitKey(0)
 
 
-def fillDissimilarityTable(imgL, imgR, dis, scanline)
-{
+"""
+def fillDissimilarityTable(imgL, imgR, dis, scanline):
+    {
 	unsigned short int y, alpha;
 
 	for (y = 0 ; y < g_cols ; y++)
@@ -51,8 +60,6 @@ def fillDissimilarityTable(imgL, imgR, dis, scanline)
 		}
         for y in range(1, img_0.shape[0]-2):
 }
-
-def matchScanlines(imgL, imgR)
-    
+def matchScanlines(imgL, imgR):
     fillDissimilarityTable(imgL, img R , dis, scanlines)
-
+"""
