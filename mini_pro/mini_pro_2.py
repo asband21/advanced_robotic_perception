c1c0a673c022a273d0b4971ca46518f4a0c0a708
asbjørn <qw41ha@student.aau.dk>
2024-10-24 22:37:56 +0200

    work in progres

--- mini_pro/mini_pro_2.py
+++ mini_pro/mini_pro_2.py
@@ -1,6 +1,53 @@
 import cv2
 import numpy as np
 
+MAXDISP = 20
+SLOP = MAXDISP + 1
+
+
+def computeIntensityGradientsX(imgL, imgR, th):
+    th = 5
+    w = 3
+
+    kernel = np.array([[-1, 0, 1]], dtype=np.float32)
+
+    fil_Il = cv2.filter2D(Il, -1, kernel)
+    fil_Ir = cv2.filter2D(Ir, -1, kernel)
+    
+    fil_Il = np.abs(fil_Il)
+    fil_Ir = np.abs(fil_Ir)
+
+    fil_Il = np.where((fil_Il <= th), 0, fil_Il)
+    fil_Ir = np.where((fil_Ir <= th), 0, fil_Ir)
+    for i in fil_Il:
+        if i < 4:
+            print(i)
+
+    return fil_Il, fil_Ir
+
+def computeIntensityGradientsX_2(imgL, imgR, th):
+    th = 5
+    w = 3
+
+    Il = imgL.copy()
+    Ir = imgL.copy()
+    print(imgl.shape)
+    for i in range(imgl.shape[0]-2):
+        sub_Il = fil_Il[i:i+w]
+        sub_Ir = fil_Il[i:i+w]
+        if(th =<  sub_Il.max() - sub_Il.min()):
+            Il[i] = 1000
+        else
+            Il[i] = 0 
+        if(th =<  sub_Ir.max() - sub_Ir.min()):
+            Ir[i] = 1000
+        else
+            Ir[i] = 0 
+
+
+
+    return Il, Ir
+
 
 delta = 20 # Maximum disparity
 κ_occ = 25 # Occlusion penalty
@@ -30,12 +77,20 @@ for i in range(1, img_0.shape[0]-2):
                 pixel_disparity[j][k-j+delta] = mids
     
     cv2.imshow("img", pixel_disparity)
-    cv2.imwrite(f"pixel_disparity_{i}.png",pixel_disparity)
+    #cv2.imwrite(f"pixel_disparity_{i}.png",pixel_disparity)
 
-    computeIntensityGradientsX(imgL, imgR, scanline, no_igL, no_igR)
+    Il_igr, Ir_igr = computeIntensityGradientsXi_2(Il, Ir, 5)
 
 
-    cv2.waitKey(0)
+    for y in range(y, img_0.shape[1]-2):
+        for delta_a in range(MAXDISP):
+            phi_best = float("inf")
+            for delta_p in range(MAXDISP):
+
+
+
+
+    #cv2.waitKey(0)
 
 cv2.imshow("img 0", img_0)
 cv2.imshow("img 1", img_1)
